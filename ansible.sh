d7e0766834ce680d2f2247bb18d4b9ccadf9ccdb
dhwarakesh <dhwarakesh31@gmail.com>
2024-12-08 20:00:28 -0500

    Updated ansible.sh to run secrets file

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,5 @@
 component=$1
 
+ansible-playbook get-secrets.yml -e vault_token=$vault_token
+
 ansible-playbook -i $component-dev.dkdevops.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense.yml -e role_name=$component

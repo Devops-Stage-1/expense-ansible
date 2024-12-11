8eeefb6deb9167a7fb2764be8c46f6521b97f788
dhwarakesh <dhwarakesh31@gmail.com>
2024-12-11 17:10:39 -0500

    Updated app.json secrets

--- ansible.sh
+++ ansible.sh
@@ -2,4 +2,4 @@ component=$1
 
 ansible-playbook get-secrets.yml -e vault_token=$vault_token
 
-ansible-playbook -i $component-dev.dkdevops.online, -e "@~/secrets.json" -e "@~/app.json" expense.yml -e role_name=$component
+ansible-playbook -i $component-dev.dkdevops.online, -e "@~/secrets.json" expense.yml -e role_name=$component

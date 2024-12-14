4e704cd3dbc6d7b4b136b5717a2566e809ad41bf
dhwarakesh <dhwarakesh31@gmail.com>
2024-12-14 17:46:27 -0500

    fixed quote issue

--- ansible.sh
+++ ansible.sh
@@ -2,4 +2,4 @@ component=$1
 
 ansible-playbook get-secrets.yml -e vault_token=$vault_token
 
-ansible-playbook -i $component-dev.dkdevops.online, -e '@~/secrets.json' -e '@~/newrelic.json' expense.yml -e role_name=$component
+ansible-playbook -i $component-dev.dkdevops.online, -e "@~/secrets.json" -e "@~/newrelic.json" expense.yml -e role_name=$component

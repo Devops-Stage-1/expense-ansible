6a2f065bafdcf0bb3ea86122583b8b8fa79b6a05
dhwarakesh <dhwarakesh31@gmail.com>
2024-12-14 17:50:21 -0500

    remove secrets after execution

--- ansible.sh
+++ ansible.sh
@@ -3,3 +3,5 @@ component=$1
 ansible-playbook get-secrets.yml -e vault_token=$vault_token
 
 ansible-playbook -i $component-dev.dkdevops.online, -e "@~/secrets.json" -e "@~/newrelic.json" expense.yml -e role_name=$component
+
+rm -f ~/secrets.json ~/newrelic.json

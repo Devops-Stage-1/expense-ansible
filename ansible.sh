4c28901fef7a62eb9b3b9b3d12dbc160e2b93221
dhwarakesh <dhwarakesh31@gmail.com>
2025-01-02 22:34:25 -0500

    update rds endpoint

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,6 @@
 component=$1
 
-ansible-playbook get-secrets.yml -e vault_token=$vault_token env=$env
+ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env
 
 ansible-playbook -i $component-dev.dkdevops.online, -e "@~/secrets.json" -e "@~/newrelic.json" -e role_name=$component expense.yml -e env=$env
 

56e9f1f9f2e5d1cbd5efbe6703e8f001108bcf48
dhwarakesh <dhwarakesh31@gmail.com>
2025-01-02 21:59:16 -0500

    Fix errors

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,7 @@
 component=$1
 
-ansible-playbook get-secrets.yml -e vault_token=$vault_token env=$env -e component=$component
+ansible-playbook get-secrets.yml -e vault_token=$vault_token env=$env
 
-ansible-playbook -i $component-dev.dkdevops.online, -e "@~/secrets.json" -e "@~/newrelic.json" expense.yml -e role_name=$component
+ansible-playbook -i $component-dev.dkdevops.online, -e "@~/secrets.json" -e "@~/newrelic.json" -e role_name=$component expense.yml -e env=$env
 
 rm -f ~/secrets.json ~/newrelic.json

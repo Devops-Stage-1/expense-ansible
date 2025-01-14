baae73846d00c4ce594c603699e2f9c9653d51ec
dhwarakesh <dhwarakesh31@gmail.com>
2025-01-14 18:57:45 -0500

    added dynamic ansible run

--- ansible.sh
+++ ansible.sh
@@ -2,6 +2,9 @@ component=$1
 
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env
 
-ansible-playbook -i $component-dev.dkdevops.online, -e "@~/secrets.json" -e "@~/newrelic.json" -e role_name=$component expense.yml -e env=$env
+#ansible-playbook -i $component-dev.dkdevops.online, -e "@~/secrets.json" -e "@~/newrelic.json" -e role_name=$component expense.yml -e env=$env
 
-rm -f ~/secrets.json ~/newrelic.json
+aws ec2 describe-instances --filters Name=tag:Name,Values=$component-$env Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
+ansible-playbook -i inv -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json'
+
+rm -f ~/*.json
